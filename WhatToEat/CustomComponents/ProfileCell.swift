ded2bfe7963aaaea185717fa67ba9a36d24177f6
Bertuğ <ylmazbertug@gmail.com>
2018-09-03 08:49:27 +0300

    Changed headerView and added profile collection cell

---
+++ WhatToEat/CustomComponents/ProfileCell.swift
@@ -0,0 +1,13 @@
+//
+//  ProfileCell.swift
+//  WhatToEat
+//
+//  Created by Bertuğ Yılmaz on 1.09.2018.
+//  Copyright © 2018 Onur Hüseyin Çantay. All rights reserved.
+//
+
+import UIKit
+
+class ProfileCell: UICollectionViewCell {
+    @IBOutlet weak var nameLabel: UILabel!
+}
