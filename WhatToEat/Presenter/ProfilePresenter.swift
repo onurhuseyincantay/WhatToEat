636641593d9a05d7a26a932b7b06731130f12683
Bertuğ <ylmazbertug@gmail.com>
2018-09-03 08:50:03 +0300

    configured presenter and viewControler

--- WhatToEat/Presenter/ProfilePresenter.swift
+++ WhatToEat/Presenter/ProfilePresenter.swift
@@ -12,21 +12,28 @@ class ProfilePresenter: NSObject {
     
     var delegate: ProfileDelegate
     
-    init(profileDelegate: ProfileDelegate) {
-        self.delegate = profileDelegate
+    init(delegate: ProfileDelegate) {
+        self.delegate = delegate
     }
 }
 
 extension ProfilePresenter: UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 1
+        return 30
     }
     
     func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        if let reusableCell = collectionView.dequeueReusableSupplementaryView(ofKind: UICollectionElementKindSectionHeader, withReuseIdentifier: "parallaxCell", for: indexPath) as? ParallaxCollectionReusableView {
+            return reusableCell
+        }
         return UICollectionReusableView()
     }
-    
+
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "profileCell", for: indexPath) as? ProfileCell {
+            cell.nameLabel.text = String(indexPath.row)
+            return cell
+        }
         return UICollectionViewCell()
     }
 }
--- WhatToEat/ViewController/ProfileViewController.swift
+++ WhatToEat/ViewController/ProfileViewController.swift
@@ -8,11 +8,28 @@
 
 import UIKit
 
-class ProfileViewController: UIViewController {
+class ProfileViewController: UICollectionViewController {
     
-    @IBOutlet weak var collectionView: UICollectionView!
+    private var profilePresenter: ProfilePresenter!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.profilePresenter = ProfilePresenter(delegate: self)
+        self.setupCollectionView()
     }
+    
+    func setupCollectionView(){
+        self.collectionView?.register(UINib(nibName: "ParallaxCollectionReusableView", bundle: nil), forSupplementaryViewOfKind: UICollectionElementKindSectionHeader, withReuseIdentifier: "parallaxCell")
+        self.collectionView?.delegate = self
+        self.collectionView?.dataSource = self.profilePresenter
+        
+        guard let width = collectionView?.bounds.width else { return }
+        let layout = collectionViewLayout as! ParallaxHeaderFlowLayout
+        layout.headerReferenceSize = CGSize(width: width, height: 300)
+        layout.itemSize = CGSize(width: width / 2, height: 60)
+    }
+}
+
+extension ProfileViewController: ProfileDelegate {
+    
 }
